7b13c8fa611ea65e80f270bcc620f72e3c96b61e
Rachelware <rachel.ware.ca@gmail.com>
2018-07-12 13:24:19 -0700

    Update dynamic.js

--- dynamic.js
+++ dynamic.js
@@ -4,11 +4,14 @@ function picChange(){
 	//alert(document.getElementById("turtle").src);
 	if (document.getElementById("turtle").src == "turtle1.png"){
 		document.getElementById("turtle").src = "turtle3.png";
+		alert("aap" + document.getElementById("turtle").src);
 	}
 	else if (document.getElementById("turtle").src == "turtle2.png"){
 		document.getElementById("turtle").src = "turtle1.png";
+		alert("oop" + document.getElementById("turtle").src);
 	}
 	else{
 		document.getElementById("turtle").src = "turtle2.png";
+		alert("eep" + document.getElementById("turtle").src);
 	}
 }
