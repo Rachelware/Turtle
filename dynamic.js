82a0d8b1fb501474d4bfe17821923c22870f50e9
Rachelware <rachel.ware.ca@gmail.com>
2018-07-12 13:26:12 -0700

    Update dynamic.js

--- dynamic.js
+++ dynamic.js
@@ -2,16 +2,13 @@ alert("alerting you of an alert that requires alert");
 
 function picChange(){
 	//alert(document.getElementById("turtle").src);
-	if (document.getElementById("turtle").src == "turtle1.png"){
+	if (document.getElementById("turtle").src == "https://rachelware.github.io/Turtle/turtle1.png"){
 		document.getElementById("turtle").src = "turtle3.png";
-		alert("aap" + document.getElementById("turtle").src);
 	}
-	else if (document.getElementById("turtle").src == "turtle2.png"){
+	else if (document.getElementById("turtle").src == "https://rachelware.github.io/Turtle/turtle2.png"){
 		document.getElementById("turtle").src = "turtle1.png";
-		alert("oop" + document.getElementById("turtle").src);
 	}
 	else{
 		document.getElementById("turtle").src = "turtle2.png";
-		alert("eep" + document.getElementById("turtle").src);
 	}
 }
